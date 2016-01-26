c6a502640e7aec235b33977f1dd6dac92af13a2c
gitLyre <guillaume.binet@lyonnaise-des-eaux.fr>
2016-01-26 17:16:07 +0100

    Ajout fichier rake migrate non pris en charge

---
+++ db/migrate/2016012614460000_create_locations.rb
@@ -0,0 +1,19 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.text :drifter_name
+      t.float :latitude
+      t.float :longitude
+      t.datetime :gps_time
+      t.datetime :time
+      t.boolean :valid_input
+      t.float :gps_speed
+      t.text :sensor_name
+      t.float :sensor_data
+      t.float :temp
+      t.float :battery_level
+      t.float :gps_tower
+      t.timestamps
+    end
+  end
+end
